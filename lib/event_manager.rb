f3e775a58381050da6037dff5b16fcbbf6457742
Reflection <reflectionoflines@gmail.com>
2023-01-24 16:10:23 -0600

    Create method to make all zipcodes a 5 digit number based on its length.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -7,8 +7,20 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+def clean_zipcode(zipcode)
+  if zipcode.nil?
+    '00000'
+  elsif zipcode.length < 5
+    zipcode.rjust(5,'0')
+  elsif zipcode.length > 5
+    zipcode[0..4]
+  else
+    zipcode
+  end
+end
+
 contents.each do |row|
   name = row[:first_name]
-  zipcode = row[:zipcode]
+  zipcode = clean_zipcode(row[:zipcode])
   puts "#{name} - #{zipcode}"
 end
