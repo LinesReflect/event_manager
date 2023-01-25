0ee256328656509a520a04f3a2aafa74094bbb82
Reflection <reflectionoflines@gmail.com>
2023-01-25 12:55:24 -0600

    Create ERB template and refactor code.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,13 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
-
-puts 'Event Manager Initialized!'
-
-contents = CSV.open(
-  'event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
-)
+require 'erb'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -21,22 +14,27 @@ def legislators_by_zipcode(zip)
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_string = legislator_names.join(", ")
+    ).officials
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
-template_letter = File.read('form_letter.html')
+puts 'Event Manager Initialized!'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
 
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
-  personal_letter = template_letter.gsub('FIRST_NAME', name)
-  personal_letter.gsub!('LEGISLATORS', legislators)
-  puts personal_letter
+  form_letter = erb_template.result(binding)
+  puts form_letter
 end
