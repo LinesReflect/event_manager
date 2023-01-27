553e420c8384b3f3b322b03bacccb3a37d7febf5
Reflection <reflectionoflines@gmail.com>
2023-01-26 22:24:44 -0600

    Gather only numbers from phone number strings.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,13 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone_number(number)
+  invalid_chars = [' ', '-', '.', '(', ')']
+  number.chars.map do |num|
+    invalid_chars.include?(num) ? '' : num
+  end
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -36,14 +43,16 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-template_letter = File.read('form_letter.erb')
-erb_template = ERB.new template_letter
+# template_letter = File.read('form_letter.erb')
+# erb_template = ERB.new template_letter
 
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
+  phone_number = clean_phone_number(row[:homephone])
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
-  form_letter = erb_template.result(binding)
-  save_thank_you_letter(id, form_letter)
+  puts "#{name}, #{phone_number}"
+  # form_letter = erb_template.result(binding)
+  # save_thank_you_letter(id, form_letter)
 end
